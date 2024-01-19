aa7d1d859dde6e6e2d067480d974cfa6cddfa774
JHN3218 <JHN3218@users.noreply.github.com>
2024-01-19 01:09:06 +0000

    Single quotes

--- permute.js
+++ permute.js
@@ -1,16 +1,16 @@
 const clog = console.log;
-const dlog = require("./silly-libs");
+const dlog = require('./silly-libs');
 
 module.exports = permute;
 function permute(arr, n = 0) {
-  perm(typeof arr == "string" ? arr.split("") : arr, n);
+  perm(typeof arr == 'string' ? arr.split('') : arr, n);
 
-  function perm(arr, n, a = "") {
+  function perm(arr, n, a = '') {
     if (n && n == a.length) {
       clog(a);
       return;
     } else if (arr.length === 1) {
-      clog(a + arr.join(""));
+      clog(a + arr.join(''));
       return;
     }
 
--- silly-libs.js
+++ silly-libs.js
@@ -1,10 +1,10 @@
-//module.exports = clog;
 const clog = console.log;
+//module.exports = clog;
 
 module.exports = dlog;
 function dlog(exprVal, ...y) {
   let output = [
-    (y.length ? "" + y[0] + (y[1] || ": ") : "") + exprVal,
+    (y.length? ''+y[0] + (y[1]||': ') :'') + exprVal,
     ...y.slice(2),
   ];
   clog(...output);
