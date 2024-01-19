b962172406feef08f82ad423e16b8e70199f56e4
JHN3218 <JHN3218@users.noreply.github.com>
2024-01-19 08:38:57 +0000

    sort input & check length

--- permute.js
+++ permute.js
@@ -2,26 +2,28 @@ const clog = console.log;
 const dlog = require('./silly-libs');
 
 module.exports = permute;
-function permute(arr, n = 0) {
+function permute(input, ln = 0) {
   let result = [];
-  (arr || arr.length) &&
-    perm(typeof arr!=='array'? (''+arr).split('') :arr, n);
+  if (input||input.length) {
+    input = (typeof input!=='array'?
+      (''+input).split('') :input).sort();
+    if (input.length<=ln) ln=0;
+    perm(input, ln);
+  }
   return result;
 
-  function perm(arr, n, a = '') {
-    if (n && n===a.length) {
-      // clog(a);
+  function perm(arr, ln, a = '') {
+    if (ln && ln===a.length) {
       result.push(a);
       return;
     } else if (arr.length===1) {
-      // clog(a + arr.join(''));
       result.push(a + arr.join(''));
       return;
     }
 
     for (let i in arr) {
-      [arr[i], arr[0]] = [arr[0], arr[i]];
-      perm(arr.slice(1), n, a + arr[0]);
+      [arr[i],arr[0]] = [arr[0],arr[i]];
+      perm(arr.slice(1), ln, a + arr[0]);
     }
   }
 }
