4795845095b26e2daf962f214d977b9f51271488
JHN3218 <JHN3218@users.noreply.github.com>
2024-01-19 03:14:52 +0000

    Check input & return output

--- permute.js
+++ permute.js
@@ -3,14 +3,19 @@ const dlog = require('./silly-libs');
 
 module.exports = permute;
 function permute(arr, n = 0) {
-  perm(typeof arr == 'string' ? arr.split('') : arr, n);
+  let result = [];
+  (arr || arr.length) &&
+    perm(typeof arr!='array'? (''+arr).split('') :arr, n);
+  return result;
 
   function perm(arr, n, a = '') {
     if (n && n == a.length) {
-      clog(a);
+      // clog(a);
+      result.push(a);
       return;
     } else if (arr.length === 1) {
-      clog(a + arr.join(''));
+      // clog(a + arr.join(''));
+      result.push(a + arr.join(''));
       return;
     }
 
