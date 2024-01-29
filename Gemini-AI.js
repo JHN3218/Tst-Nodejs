57d4bb523582d1801a30e874b7f3c1baace2b7d7
JHN3218 <JHN3218@users.noreply.github.com>
2024-01-29 09:58:31 +0000

    Add & Test config & safety-setting

--- Gemini-AI.js
+++ Gemini-AI.js
@@ -1,19 +1,65 @@
-const { GoogleGenerativeAI } = require("@google/generative-ai");
+const { GoogleGenerativeAI, HarmBlockThreshold, HarmCategory } = require('@google/generative-ai');
 
 // Access your API key as an environment variable (see "Set up your API key" above)
-const genAI = new GoogleGenerativeAI(process.env["Gemini_API_ky"]);
+const genAI = new GoogleGenerativeAI(process.env['Gemini_API_ky']);
 
-module.exports.GeminiTxt =
+module.exports.GeminiAI =
 async (prompt) => {
   if (!prompt) {
-    console.log("No prompt provided.");
+    console.log('No prompt provided.');
     return;
   }
   // For text-only input, use the gemini-pro model
-  const model = genAI.getGenerativeModel({ model: "gemini-pro" });
+  const generationConfig = {
+    // stopSequences: ["red"],
+    // maxOutputTokens: 200,
+    temperature: 0.9, //0-.9
+    topK: 5,
+    // topP: 1, //.95
+  };
 
-  const result = await model.generateContent(prompt);
-  const response = await result.response;
-  const text = response.text();
-  console.log(text);
+const safetySettings = [
+  {
+    category: HarmCategory.HARM_CATEGORY_HARASSMENT,
+    threshold: HarmBlockThreshold.BLOCK_NONE,
+  },
+  {
+    category: HarmCategory.HARM_CATEGORY_HATE_SPEECH,
+    threshold: HarmBlockThreshold.BLOCK_NONE,
+  },
+{
+    category: HarmCategory.HARM_CATEGORY_SEXUALLY_EXPLICIT,
+    threshold: HarmBlockThreshold.BLOCK_NONE,
+  },
+  {
+    category: HarmCategory.HARM_CATEGORY_DANGEROUS_CONTENT,
+    threshold: HarmBlockThreshold.BLOCK_NONE,
+  },
+// {
+//   category: HarmCategory.HARM_CATEGORY_DEROGATORY,
+//   threshold: HarmBlockThreshold.BLOCK_NONE,
+// },
+// {
+//   category: HarmCategory.HARM_CATEGORY_TOXICITY,
+//   threshold: HarmBlockThreshold.BLOCK_NONE,
+// },
+// {
+//   category: HarmCategory.HARM_CATEGORY_SEXUAL,
+//   threshold: HarmBlockThreshold.BLOCK_NONE,
+// },
+];
+
+  const model = genAI.getGenerativeModel({
+    model: "gemini-pro",
+    generationConfig,
+    safetySettings,
+  });
+
+  const result = await model.generateContentStream(prompt);
+  for await (const chunk of result.stream) {
+    console.log(chunk.text());
+  }
+  // const response = await result.response;
+  // const text = response.text();
+  // console.log(text);
 };
--- index.js
+++ index.js
@@ -1,9 +1,10 @@
 const clog = console.log;
 const {dlog} = require('./silly-libs');
 const {permute} = require('./permute');
-const {GeminiTxt} = require('./Gemini-AI');
+const {GeminiAI} = require('./Gemini-AI');
 
 //clog(dlog(123,'abc'));
 //clog(!(''||([]).length||null));
 //clog(permute('1726&£@fg',3).join('\n'));
-GeminiTxt('How the sky is blue?');
+const prompt = "based on neurologist journal, what is female neurotic response while her breast being caress."
+GeminiAI(prompt);
