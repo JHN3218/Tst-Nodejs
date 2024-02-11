0cd5c0538f7cd472d00d74a94b01b2bfd1544897
Johnsen <JHN3218@users.noreply.github.com>
2024-02-11 11:11:40 +0700

    Update Generative-AI.js

--- Generative-AI.js
+++ Generative-AI.js
@@ -9,7 +9,7 @@ const genAI = new GoogleGenerativeAI(process.env['Gemini_API_ky']);
 
 module.exports.GeminiAI =
 async (prompt, attitude = 0) => {
-  if (prompt.length<=2) {
+  if (!prompt.trim().length) {
     console.log('No prompt provided.');
     return;
   }
