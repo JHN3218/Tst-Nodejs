a9b9204c6ddf10ed9a0dcb94392dd81d2ba21660
JHN3218 <JHN3218@users.noreply.github.com>
2024-02-13 13:46:48 +0000

    Testing DeepMerge

--- index.js
+++ index.js
@@ -19,12 +19,17 @@ const {
 
 //clog(dlog(123,'abc'));
 //clog(!(''||([]).length||null));
-//clog(permute('1726&£@fg',3).join('\n'));
+//clog(permute('172',2).join('\n'));
 // JSON.parse(jsonData))
-//clog(is(56).Between(0,100));
+//loop(10,()=>dlog(is(dlog(Math.round(Math.random()*100))).inRange(25,75)));
 a=[{a:1,b:2,c:[7,8,9],hh:[5,5,5]}];
-//dlog(MergeObjs(1,1)(a,[{c:{x:1,y:2,z:3},hh:loop(10,(i,r)=>r.push(i**(i/(i/2))||0),[])}]),
-//   'final output:');
+dlog(MergeObjs(0,1)(
+      a,
+      [{c:{x:1,y:2,z:3},hh:loop(10,(i,r)=>r.push(i**(i/(i/2))||0),[])}],
+      [4,5,6],
+  {a:1,b:2,c:3},
+     ),
+    'final output:');
 // clipboardy.writeSync('Hello World');
 
 
