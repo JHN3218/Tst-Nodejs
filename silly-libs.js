dd3d133fa8a9133db2d11284982920fc6bcbdd33
JHN3218 <JHN3218@users.noreply.github.com>
2024-02-13 13:45:42 +0000

    Bug fix DeepMerge + MergeFunc feature

--- silly-libs.js
+++ silly-libs.js
@@ -18,44 +18,66 @@ function test(f) {
 }
 
 function MergeObjs(add = false, replace = false) {
-  var result;
+  let result;
+  let MergeFunc = DeepMerge;
   return (...objs) => {
-    for (const obj of objs) {
-      if (!is(result).Object()) result = is(obj).Array() ? [] : {};
-      result = DeepMerge(result, obj);
+    result = structuredClone(objs[0]);
+    if (1 < objs.length) {
+      if (is(add).Function()) MergeFunc = DeepMergeFunc;
+      for (const obj of objs.slice(1)) result = MergeFunc(result, obj);
     }
     return result;
   };
 
-  function DeepMerge(obj1, obj2) {
-    let obj = structuredClone(obj1);
-    for (const [ky, val] of Object.entries(obj2)) {
-      if (!add && !replace && ky in obj) continue;
+  function DeepMerge(result, obj) {
+    for (const [ky, val] of Object.entries(obj)) {
+      if (!add && !replace && ky in result) continue;
+      if (add && !replace && is(result, obj).Array()) {
+        result.push(...obj);
+        break;
+      }
       if (!is(val).Object())
-        obj[ky] =
+        result[ky] =
           add && replace
-            ? is(obj[ky]).Number()
-              ? obj[ky] + val
-              : val
+            ? is(result[ky], val).RealNum()
+              ? result[ky] + val
+              : is(result[ky]).Object()
+                ? DeepMerge(
+                    result[ky],
+                    Array.isArray(val)
+                      ? (((a = [])[ky] = val), a)
+                      : { [ky]: val },
+                  )
+                : val
             : add
-              ? (obj[ky] || 0) + val
+              ? (result[ky] || 0) + val
               : val;
       else
-        obj[ky] =
+        result[ky] =
           add && !replace
-            ? Array.isArray(val)
-              ? [...(obj[ky] || []), ...val]
-              : DeepMerge(obj[ky] || {}, val)
-            : DeepMerge(obj[ky] || (Array.isArray(val) && []) || {}, val);
+            ? is(result[ky], val).Array()
+              ? [...(result[ky] || []), ...val]
+              : DeepMerge(result[ky] || {}, val)
+            : DeepMerge(result[ky] || (Array.isArray(val) && []) || {}, val);
     }
-    return obj;
+    return result;
+  }
+
+  function DeepMergeFunc(result, obj) {
+    for (const [ky, val] of Object.entries(obj)) {
+      if (!replace && ky in result) continue;
+      result[ky] = !is(result[ky], val).Object()
+        ? add(result, ky, val, replace)
+        : DeepMergeFunc(result[ky] || (Array.isArray(val) && []) || {}, val);
+    }
+    return result;
   }
 }
 
 function loop(ln, func, returnVal = 0) {
   let cond = { exit: false };
   let [i, end, step] = [ln[0] || 0, ln[1] || ln, ln[2] || 1];
-  for (; !cond.exit && i < end; i += step) returnVal = func(i, returnVal, cond);
+  for (; !cond.exit && i < end; i += step) func(i, returnVal, cond);
   return returnVal;
 }
 
@@ -63,19 +85,30 @@ function is(...x) {
   const members = {
     Any: (...y) => x.some((v) => y.includes(v)),
     All: (...y) => x.every((v) => y.includes(v)),
-    Between: (lo, hi) => x.every((v) => lo < v < hi),
-    inRange: (lo, hi) => x.every((v) => lo <= v <= hi),
+    Between: (lo, hi) => x.every((v) => lo < v && v < hi),
+    inRange: (lo, hi) => x.every((v) => lo <= v && v <= hi),
     Undefined: () => x.every((v) => typeof v === "undefined"),
     Nøl: () => x.every((v) => !v || !v.length),
     Boolean: () => x.every((v) => typeof v === "boolean"),
     Primitive: () => x.every((v) => typeof v !== "object"),
-    Number: () => x.every((v) => typeof v === "number"),
-    RealNum: () => x.every((v) => typeof v === "number" && !isNaN(v)),
-    Integer: () => x.every((v) => typeof v === "number" && Number.isInteger(v)),
-    Floating: () => x.every((v) => typeof v === "number" && !Number.isInteger(v)),
+    Number: () => x.every((v) => typeof +v === "number"),
+    RealNum: () =>
+      x.every((v) => ((v = +v), typeof v === "number" && !isNaN(v))),
+    Integer: () =>
+      x.every(
+        (v) => (
+          (v = +v), typeof v === "number" && Number.isInteger(v) && !isNaN(v)
+        ),
+      ),
+    Floating: () =>
+      x.every(
+        (v) => (
+          (v = +v), typeof v === "number" && !Number.isInteger(v) && !isNaN(v)
+        ),
+      ),
     // Complex: () => x.every((v) => typeof v === "number" && !Number.isInteger(v) && !isNaN(v)),
     // imaginary: () => x.every((v) => typeof v === "number" && !Number.isInteger(v) && !isNaN(v) && v.imaginary),
-    NaN: () => x.every((v) => typeof v === "number" && isNaN(v)),
+    NaN: () => x.every((v) => ((v = +v), typeof v === "number" && isNaN(v))),
     String: () => x.every((v) => typeof v === "string"),
     Symbol: () => x.every((v) => typeof v === "symbol"),
     Array: () => x.every((v) => Array.isArray(v)),
