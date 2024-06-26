943910095c478cd7a861ffb17ad33051e81cdb67
Johnsen <JHN3218@users.noreply.github.com>
2024-06-26 12:17:24 +0000

    require statistics

--- index.js
+++ index.js
@@ -15,6 +15,12 @@ const
   tst,
 } = require('./silly-math-libs')
 ,{
+  min,
+  max,
+  mean,
+  // median,
+} = require('./silly-statistics-libs')
+,{
   GeminiAI,
   // OpenAI,
 } = require('./Generative-AI')
@@ -42,26 +48,5 @@ R² in statistic
 // GeminiAI(prompt);
 // OpenAI(prompt);
 
-
-// provide optional config object (or undefined). Defaults shown.
-const config = {
-  binaryThresh: 0.5,
-  hiddenLayers: [3], // array of ints for the sizes of the hidden layers in the network
-  activation: 'sigmoid', // supported activation types: ['sigmoid', 'relu', 'leaky-relu', 'tanh'],
-  leakyReluAlpha: 0.01, // supported for activation type 'leaky-relu'
-};
-
-// create a simple feed-forward neural network with backpropagation
-/* const net = new brain.NeuralNetwork(config);
-// train the network
-net.train([
-  { input: [0, 0], output: [0] },
-  { input: [0, 1], output: [1] },
-  { input: [1, 0], output: [1] },
-  { input: [1, 1], output: [0] },
-]);
-
-const output = net.run([1, 0]); // [0.987]
-*/
-
 // tst(.5)
+
