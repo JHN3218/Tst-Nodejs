b6e4a1d6b939f388fc3c990a046d359bd04b5279
Johnsen <JHN3218@users.noreply.github.com>
2024-06-26 18:29:03 +0000

    add2mean, median

--- silly-statistics-libs.js
+++ silly-statistics-libs.js
@@ -1,24 +1,41 @@
-const min = arr => {
+const min = arr =>
+  arr.length?
+    arr.reduce((r,i) => Math.min(r,i))
+  :undefined;
+/* fast {
   if (!arr.length) return undefined;
   if (arr.length===1) return arr[0];
   var result = arr[0];
   for (let i=1; i<arr.length; i++)
     result = Math.min(result,arr[i]);
   return result;
-}
+} */
 
-const max = arr => {
+const max = arr =>
+  arr.length?
+    arr.reduce((r,i) => Math.max(r,i))
+  :undefined;
+/* fast {
   if (!arr.length) return undefined;
   if (arr.length===1) return arr[0];
   var result = arr[arr.length-1];
   for (let i=arr.length-2; 0<i; i--)
     result = Math.max(result,arr[i]);
   return result;
-}
+} */
 
 const mean = arr =>
   arr.length?
     arr.Σ() / arr.length
   :undefined;
 
-module.exports = { min, max, mean };
+const add2mean = (mean, ln, nv) =>
+  mean + (nv-mean)/(ln+1);
+  // (mean*ln + nv) / (ln+1);
+
+const median = arr =>
+  arr.length?
+    arr.sort((a,b)=>a-b)[0|arr.length/2]
+  :undefined;
+
+module.exports = { min, max, mean, add2mean, median };
