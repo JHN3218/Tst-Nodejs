295cc6b0bd4bfa25a4f9957927bef51fa15c729c
Johnsen <JHN3218@users.noreply.github.com>
2024-06-26 18:26:46 +0000

    Anthropic

--- Generative-AI.js
+++ Generative-AI.js
@@ -1,9 +1,12 @@
+const { fs } = require('fs');
+
+const sysRole = 'As expert analysis in complete details with technical straight concise formal academic style';
+
 const {
   GoogleGenerativeAI,
   HarmBlockThreshold,
   HarmCategory,
-} = require('@google/generative-ai')
-,{ fs } = require('fs');
+} = require('@google/generative-ai');
 
 // Access your API key as an environment variable (see "Set up your API key" above)
 const genAI = new GoogleGenerativeAI(process.env['Gemini_API_ky']);
@@ -11,7 +14,7 @@ const genAI = new GoogleGenerativeAI(process.env['Gemini_API_ky']);
 module.exports.GeminiAI =
 async (
     prompt,
-    role = `As expert analysis in complete details with technical concise straight formal academic format`,
+    role = '',
     attitude = 0
 ) => {
   if (!prompt.trim().length) {
@@ -81,11 +84,12 @@ async (
     safetySettings,
   });
 
+  if (!role) role = sysRole;
   const result = await model.generateContentStream(role + ':\n' + prompt);
   for await (const chunk of result.stream) {
     console.log(chunk.text());
   }
-  // const response = await result.response;
+  // const response = await reprocess.env['Anthropic_API_ky']sult.response;
   // const text = response.text();
   // console.log(text);
 };
@@ -98,13 +102,13 @@ const openai = new OpenAI({apiKey:process.env['OpenAI_API_ky']});
 module.exports.OpenAI =
 async (
   prompt,
-  system = 'As expert analysis in complete details with technical concise straight formal academic format',
+  role = '',
 ) => {
   const completion = await openai.chat.completions.create({
     messages: [
       {
         role: "system",
-        content: system,
+        content: role || sysRole,
       },
     {
       role: "user",
@@ -115,4 +119,38 @@ async (
   });
 
   console.log(completion.choices[0]);
+}
+
+
+const Anthropic = require("@anthropic-ai/sdk");
+
+const anthropic = new Anthropic({['x-api-key']:process.env['Anthropic_API_ky']});
+
+module.exports.ClaudeAI =
+async (
+  prompt,
+  role = '',
+  modl = 'Sonnet',
+) => {
+  const model = {
+    Opus: 'claude-3-opus-20240229',
+    Sonnet: 'claude-3-5-sonnet-20240620',
+    Haiku: 'claude-3-haiku-20240307',
+  };
+  await anthropic.messages.stream({
+    model: model[modl],
+    max_tokens: 1024,
+    temperature: 0, // 0.0 (analytic) - 1.0 (creative)
+    // stream: true,
+    system: role || sysRole,
+    messages: [{
+      "role": "user",
+      "content": [{
+        "type": "text",
+        "text": prompt
+      }]
+    }]
+  }).on('text', (text) => {
+    console.log(text);
+  });
 }
